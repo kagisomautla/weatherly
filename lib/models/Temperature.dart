d3e9379055f97ec4aec37e0347fc75c4d300d4eb
Bambino95 <kagiso@doshex.com>
2024-03-22 22:00:24 +0200

    Temperature model

---
+++ lib/models/Temperature.dart
@@ -0,0 +1,47 @@
+class Temperature {
+  final double? temp;
+  final double? feelsLike;
+  final double? tempMin;
+  final double? tempMax;
+  final int? pressure;
+  final int? humidity;
+  final int? seaLevel;
+  final int? groundLevel;
+
+  Temperature({
+    this.temp,
+    this.feelsLike,
+    this.tempMin,
+    this.tempMax,
+    this.pressure,
+    this.humidity,
+    this.seaLevel,
+    this.groundLevel,
+  });
+
+  factory Temperature.fromJson(Map<String, dynamic> json) {
+    return Temperature(
+      temp: json['temp'].toDouble(),
+      feelsLike: json['feels_like'].toDouble(),
+      tempMin: json['temp_min'].toDouble(),
+      tempMax: json['temp_max'].toDouble(),
+      pressure: json['pressure'],
+      humidity: json['humidity'],
+      seaLevel: json['sea_level'],
+      groundLevel: json['grnd_level'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['temp'] = temp;
+    data['feels_like'] = feelsLike;
+    data['temp_min'] = tempMin;
+    data['temp_max'] = tempMax;
+    data['pressure'] = pressure;
+    data['humidity'] = humidity;
+    data['sea_level'] = seaLevel;
+    data['grnd_level'] = groundLevel;
+    return data;
+  }
+}
