930b97272ec490bba1026e09c7a3017009364493
Bambino95 <kagiso@doshex.com>
2024-04-03 23:17:00 +0200

    Update landing_screen.dart

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,6 +1,12 @@
 import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:weatherly/constants/colors.dart';
+import 'package:weatherly/screens/no_internet_connection_screen.dart';
+import 'package:weatherly/services/local_storage_service.dart';
+import 'package:weatherly/viewmodels/weather_view_model.dart';
+import 'package:http/http.dart' as http;
 
 class LandingScreen extends StatefulWidget {
   const LandingScreen({super.key});
@@ -10,18 +16,56 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
-  init() {
-    int seconds = 1;
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        if (seconds > 0) {
-          seconds--;
+  init() async {
+    bool hasOnboarded = await LocalStorageService.getOnboardingStatus();
+    final weatherViewModel = Provider.of<WeatherViewModel>(context, listen: false);
+
+    if (hasOnboarded) {
+      //fetch last updated weather data from local storage
+      await weatherViewModel.fetchOfflineWeatherInformation(context: context);
+
+      print('Has onboarded... Take to welcome screen');
+      //navigate to welcome screen
+      Navigator.pushNamedAndRemoveUntil(context, '/navigator', (route) => false);
+    } else {
+      //navigate to onboarding screen
+      print('Has NOT  onboarded... Take to onboading screen');
+      await checkForInternetConnectivity().then((value) {
+        if (value) {
+          Navigator.pushNamedAndRemoveUntil(context, '/onboarding', (route) => false);
         } else {
-          Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
-          timer.cancel();
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => NoInternetConnectionScreen(
+                showRetryButton: true,
+                onRetry: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LandingScreen()));
+                },
+              ),
+            ),
+          );
         }
       });
-    });
+    }
+  }
+
+  Future<bool> checkForInternetConnectivity() async {
+    final List<ConnectivityResult> connectivityResult = await (Connectivity().checkConnectivity());
+
+    bool isConnected = connectivityResult.contains(ConnectivityResult.mobile) || connectivityResult.contains(ConnectivityResult.wifi);
+
+    if (!isConnected) {
+      return false;
+    } else {
+      //check if the device has internet connection by doing a dummy search
+      final response = await http.get(Uri.parse('https://www.google.com'));
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 
   @override
@@ -31,9 +75,15 @@ class _LandingScreenState extends State<LandingScreen> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kSunny,
+      backgroundColor: kSunnyBackgroundColor,
       body: Center(
         child: Container(
           clipBehavior: Clip.antiAlias,
