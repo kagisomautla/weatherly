3693836bc89b14eaa9109eb8de60b39a8a4a491c
Bambino95 <kagiso@doshex.com>
2024-04-03 23:17:16 +0200

    Update Temperature.dart

--- lib/models/Temperature.dart
+++ lib/models/Temperature.dart
@@ -1,47 +1,48 @@
-class Temperature {
+class TemperatureModel {
   final double? temp;
-  final double? feelsLike;
   final double? tempMin;
   final double? tempMax;
+  final double? feelsLike;
+  final double? humidity;
   final int? pressure;
-  final int? humidity;
   final int? seaLevel;
-  final int? groundLevel;
+  final int? grndLevel;
+  
 
-  Temperature({
+  TemperatureModel({
     this.temp,
-    this.feelsLike,
     this.tempMin,
     this.tempMax,
-    this.pressure,
+    this.feelsLike,
     this.humidity,
+    this.pressure,
     this.seaLevel,
-    this.groundLevel,
+    this.grndLevel,
   });
 
-  factory Temperature.fromJson(Map<String, dynamic> json) {
-    return Temperature(
-      temp: json['temp'].toDouble(),
-      feelsLike: json['feels_like'].toDouble(),
-      tempMin: json['temp_min'].toDouble(),
-      tempMax: json['temp_max'].toDouble(),
+  factory TemperatureModel.fromJson(Map<String, dynamic> json) {
+    return TemperatureModel(
+      temp: double.parse(json['temp'].toString()),
+      tempMin: double.parse(json['temp_min'].toString()),
+      tempMax: double.parse(json['temp_max'].toString()),
+      feelsLike: double.parse(json['feels_like'].toString()),
+      humidity: double.parse(json['humidity'].toString()),
       pressure: json['pressure'],
-      humidity: json['humidity'],
-      seaLevel: json['sea_level'],
-      groundLevel: json['grnd_level'],
+      seaLevel: json['sea_level'] ?? 0,
+      grndLevel: json['grnd_level'] ?? 0,
     );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['temp'] = temp;
-    data['feels_like'] = feelsLike;
-    data['temp_min'] = tempMin;
-    data['temp_max'] = tempMax;
-    data['pressure'] = pressure;
-    data['humidity'] = humidity;
-    data['sea_level'] = seaLevel;
-    data['grnd_level'] = groundLevel;
-    return data;
+    return {
+      'temp': temp,
+      'temp_min': tempMin,
+      'temp_max': tempMax,
+      'feels_like': feelsLike,
+      'humidity': humidity,
+      'pressure': pressure,
+      'sea_level': seaLevel,
+      'grnd_level': grndLevel,
+    };
   }
 }
