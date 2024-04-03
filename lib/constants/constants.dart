91670179fac7200ac87dfdfb784e22c0398ed897
Bambino95 <kagiso@doshex.com>
2024-04-03 23:09:15 +0200

    constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,17 @@
+//local storage keys
+const String kForecastDataKey = 'forecastData';
+const String kWeatherDataKey = 'weatherData';
+const String kLocationDataKey = 'locationData';
+const String kFavouriteLocationDataKey = 'favouriteLocationData';
+const String kCoordinatesDataKey = 'coordinatesData';
+const String kCurrentLocationDataKey = 'currentLocationData';
+const String kOnboardingStatusKey = 'onboardingStatus';
+const String kSearchedLocationDataKey = 'search';
+
+const double cPadding = 12.0;
+
+const String kSunnyBackgroundImage = 'assets/images/sea_sunny.png';
+const String kCloudyBackgroundImage = 'assets/images/sea_cloudy.png';
+const String kRainyBackgroundImage = 'assets/images/sea_rainy.png';
+
+const String kPlaceholderImageUrl = 'https://placehold.co/600x400';
